8287687b71c4f82744b909f5ccd93e16c8e3d745
nac19 <74272103+nac19@users.noreply.github.com>
2023-11-18 05:54:02 +0300

    nft expiration & ccip func

--- contracts/Main.sol
+++ contracts/Main.sol
@@ -8,21 +8,45 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract NFT is ERC721, Ownable {
     uint256 private _nextTokenId;
+    uint256 immutable expirationTime;
+    mapping(uint256 => uint256) expirationDates;
 
-    constructor(string memory _tokenName)
+    constructor(string memory _tokenName, uint256 _expirationTime)
         ERC721(_tokenName, "PRV")
         Ownable(msg.sender)
-    {}
+    {
+        expirationTime = _expirationTime;
+    }
 
     function safeMint(address to) public onlyOwner {
         uint256 tokenId = _nextTokenId++;
+        if(expirationTime > 0){
+            expirationDates[tokenId] = block.timestamp + expirationTime;
+        }
         _safeMint(to, tokenId);
+    }
+
+    function getExpireDate(uint256 _tokenID) external view returns(uint256) {
+        return expirationDates[_tokenID];
+    }
+
+    function isExpired(uint256 _tokenID) external view returns(bool){
+        if(expirationTime > 0){
+            if(block.timestamp < expirationDates[_tokenID]){
+                return false;
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
     }
 }
 
 contract main{
     IERC20 immutable NATIVE;
     address immutable owner;
+    address immutable CCIP;
     mapping(address => bool) solvers;
     Item[] items;
 
@@ -36,6 +60,11 @@ contract main{
         _;
     }
 
+    modifier onlyCCIP {
+        require(msg.sender == CCIP);
+        _;
+    }
+
     struct Item {
         address publisher;
         uint256 price;
@@ -44,14 +73,15 @@ contract main{
         NFT nftaddress;
     }
 
-    constructor(IERC20 _native){
+    constructor(IERC20 _native, address _CCIPaddress){
         owner = msg.sender;
         NATIVE = _native;
+        CCIP = _CCIPaddress;
     }
 
-    function addItem(string memory _itemName, string memory _nftShortname, uint256 _price) external returns(uint256 itemID) {
+    function addItem(string memory _itemName, string memory _nftShortname, uint256 _price, uint256 _expirationTime) external returns(uint256 itemID) {
         require(bytes(_itemName).length > 0 && bytes(_nftShortname).length > 0);
-        NFT nftaddress = new NFT(_itemName, _nftShortname);
+        NFT nftaddress = new NFT(_itemName, _expirationTime);
         Item memory newitem = Item(msg.sender, _price, 0, _itemName, nftaddress);
         items.push(newitem);
         return (items.length - 1);
@@ -82,11 +112,15 @@ contract main{
         (temp.nftaddress).safeMint(msg.sender);
     }
 
-    function lib(address _userAddress, uint256 _gameID) external view returns(bool){
-        if((items[_gameID].nftaddress).balanceOf(_userAddress) > 0){
+    function lib(address _userAddress, uint256 _itemID) external view returns(bool){
+        if((items[_itemID].nftaddress).balanceOf(_userAddress) > 0){
             return true;
         } else {
             return false;
         }
+    }
+
+    function CCIP_mint(address _buyer, uint256 _itemID) external onlyCCIP{
+        (items[_itemID].nftaddress).safeMint(_buyer);
     }
 }
